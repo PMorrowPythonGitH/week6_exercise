7aec2e5c9535992d59d7e439aaf54fb6e9992b81
Paul Morrow <pmorrow1@yahoo.ca>
2023-03-07 19:04:18 -0500

    Create person.py

---
+++ person.py
@@ -0,0 +1,9 @@
+class Person:
+
+    def __init__(self, f_name, l_name):
+      self.f_name = f_name
+      self.l_name = l_name
+      
+    def get_full_name(self):
+      return f'{self.f_name} {self.l_name}
+      
